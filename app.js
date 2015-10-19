8d72a7c7c431119357d7457929003040f5833cd7
web2solutions <eduardo@web2solutions.com.br>
2015-10-19 15:25:59 -0200

    server

--- app.js
+++ app.js
@@ -7,39 +7,44 @@ var redis = require('redis');
 var redis_client = redis.createClient();
 
 var port = 4080;
- 
+
 
 redis_client.on('connect', function() {
-    console.log('redis connected');
+	console.log('redis connected');
 });
 
-app.use(function (req, res, next) {
-  //console.log('middleware');
-  req.testing = 'testing';
-  return next();
+app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next) {
+	//console.log('middleware');
+	req.testing = 'testing';
+	return next();
 });
 
-
- 
-app.get('/', function(req, res, next){
-  res.send('Hello World!');
-  //console.log('get route', req.testing);
-  res.end();
+app.get('/', function(req, res, next) {
+	res.send('Hello World!');
+	//console.log('get route', req.testing);
+	res.end();
 });
 
-
- 
 app.ws('/', function(ws, req) {
-  ws.on('message', function(msg) {
-    console.log(msg);
-  });
-  console.log('socket', req.testing);
+
+
+	// client.set('client_', 'clientid');
+	//client.set('framework', 'AngularJS', function(err, reply) {
+	//  console.log(reply);
+	//});
+
+	ws.on('message', function(msg) {
+		console.log(msg);
+	});
+	console.log('socket', req.testing);
 });
- 
 
-var server = app.listen(port, function () {
-  var host = server.address().address;
-  var port = server.address().port;
 
-  console.log( app_name + ' is listening at http://%s:%s', host, port );
+var server = app.listen(port, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log(app_name + ' is listening at http://%s:%s', host, port);
 });
